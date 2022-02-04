a1e011c8d1cb293dc3710186ab5f82c0c3b2579a
kaleb borda <kaleb.borda@expermed.com.br>
2022-02-04 07:22:21 -0300

    [FEATURE] Remoçao de bloqueio cors

--- api/main.go
+++ api/main.go
@@ -4,12 +4,15 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"github.com/kalberfon/buscaCep-go-react/api/controllers"
+	"github.com/kalberfon/buscaCep-go-react/api/middlewares"
 )
 
 func main() {
 	godotenv.Load(".env")
-	r := gin.Default()
-	r.GET("/:cep", controllers.GetSearchCep)
+	router := gin.Default()
 
-	r.Run()
+	router.Use(middlewares.AllowCorsAllOrigin)
+	router.GET("/:cep", controllers.GetSearchCep)
+
+	router.Run(":8000")
 }
---
+++ api/middlewares/allowcors.go
@@ -0,0 +1,9 @@
+package middlewares
+
+import "github.com/gin-gonic/gin"
+
+func AllowCorsAllOrigin(c *gin.Context) {
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	c.Header("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Authorization")
+}
